bb35c49d83de134e0e9cba0155b516d5ae5cfded
[aateka limbaliya] <[aatekalimbaliya910@gmail.com]>
2025-07-24 16:45:42 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/blog-web-live",
+  base: "/goops-blog/",
   build: {
     rollupOptions: {
       external: ['react-router-dom']
